a8c5494d27090bfbb364c9ae16c37d83ccdff8f0
SyedTousif <stousif1020@gmail.com>
2024-11-08 16:24:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ resource "azurerm_app_service_plan" "example" {
     size = "B1"
   }
 }
+resource "azurerm_app_service" "example" {
+  name                = "deployment-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "NODE|16"
+  }
+}
