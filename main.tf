5948ea305762052a34ae46f973e5096a9f9a3451
SyedTousif <stousif1020@gmail.com>
2024-11-11 17:39:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "syedpavan-rg"
-  location = "West Europe"
+  location = "South India"
 }
 
 resource "azurerm_service_plan" "example" {
@@ -12,7 +12,7 @@ resource "azurerm_service_plan" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type             = "Linux"    # Required field for OS type
-  sku_name            = "P1v2"     # Required SKU name
+  sku_name            = "S1"     # Required SKU name
 }
 
 resource "azurerm_linux_web_app" "example" {
