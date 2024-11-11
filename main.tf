ee1edb1e564cab05f7abf785d25b6bae3503e3ae
SyedTousif <stousif1020@gmail.com>
2024-11-11 12:02:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "azurerm_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  os_type             = "Linux"                    # Required field for OS type
-  sku_name            = "P1v3"                     # Required SKU name, replace if you want a different size
+  os_type             = "Linux"    # Required field for OS type
+  sku_name            = "P1v3"     # Required SKU name
 }
 
 resource "random_id" "suffix" {
@@ -23,9 +23,10 @@ resource "azurerm_app_service" "example" {
   name                = "example-appservice-${random_id.suffix.hex}" # Unique name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_service_plan.example.id             # Correct reference
+  app_service_plan_id = azurerm_service_plan.example.id              # Correct reference to service plan
 
   site_config {
     # Add any relevant site configurations here
   }
 }
+
