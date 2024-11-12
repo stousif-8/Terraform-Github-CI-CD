c8579a65b036724ff91eea1909f37c7af1ea5ea2
SyedTousif <stousif1020@gmail.com>
2024-11-12 10:26:04 +0530

    Create azureapp-service.tf

---
+++ azureapp-service.tf
@@ -0,0 +1,10 @@
+resource "azurerm_linux_web_app" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {
+    # Add any relevant site configurations here
+  }
+}
