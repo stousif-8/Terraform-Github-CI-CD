1aed6ab414b4fdfe413a0ad9a44728f0cbea0a4b
SyedTousif <stousif1020@gmail.com>
2024-11-12 09:58:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "azurerm_service_plan" "example" {
   name                = "example"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  os_type             = "Linux"    # Required field for OS type
-  sku_name            = "P1v3"     # Required SKU name
+  os_type             = "Linux"
+  sku_name            = "P1v3"
 }
 
 resource "azurerm_linux_web_app" "example" {
@@ -25,5 +25,3 @@ resource "azurerm_linux_web_app" "example" {
     # Add any relevant site configurations here
   }
 }
-
-
