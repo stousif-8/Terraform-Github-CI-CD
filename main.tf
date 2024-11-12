00489bdf6ad8bbdd6c046229a3e13a93c5b6062b
SyedTousif <stousif1020@gmail.com>
2024-11-12 10:25:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,14 +14,3 @@ resource "azurerm_service_plan" "example" {
   os_type             = "Linux"
   sku_name            = "P1v3"
 }
-
-resource "azurerm_linux_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
-
-  site_config {
-    # Add any relevant site configurations here
-  }
-}
